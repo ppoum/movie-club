2f5b92c5b5fdd61c3ed5dcff6fbb1d97ec184b28
ppoum <pam@poum.ca>
2025-12-29 20:39:06 -0500

    Create new theme.css file with common background colors

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+// TODO: move to styles directory?
 import "./index.css";
+import "./styles/theme.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
