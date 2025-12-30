c9d8985713d2cb9ccdaf22e171bb5f6e8d291d8c
ppoum <pam@poum.ca>
2025-12-30 14:59:47 -0500

    Move index.css to styles directory

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-// TODO: move to styles directory?
-import "./index.css";
+import "./styles/index.css";
 import "./styles/theme.css";
 import App from "./App.tsx";
 
